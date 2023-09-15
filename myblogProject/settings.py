1c9c5cf9f8644145512f00cb9c3281061edcc9dd
amankayat <72142181+amankayat@users.noreply.github.com>
2023-09-15 19:56:57 +0530

    Update settings.py

--- myblogProject/settings.py
+++ myblogProject/settings.py
@@ -155,5 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'amankayat789@gmail.com'
-EMAIL_HOST_PASSWORD = 'ozyhfwlbrneooqqk'
+EMAIL_HOST_USER = 'your_email'
+EMAIL_HOST_PASSWORD = 'password'
